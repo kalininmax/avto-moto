51195457f9049fc5b00eb4f86561d4d1225a1527
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-10-10 01:17:05 +0300

    Добавляет табы с характеристиками и контактами

--- src/components/product-screen/product-screen.jsx
+++ src/components/product-screen/product-screen.jsx
@@ -3,9 +3,12 @@ import { Helmet } from 'react-helmet'
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Gallery from '../gallery/gallery';
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
+
 import OpenSansRegular from '../../fonts/opensans-regular.woff2';
 import OpenSansSemiBold from '../../fonts/opensans-semibold.woff2';
 import OpenSansBold from '../../fonts/opensans-bold.woff2';
+import "@reach/tabs/styles.css";
 import './product-screen.scss';
 
 import { PRODUCT } from '../../data';
@@ -40,6 +43,48 @@ function ProductScreen() {
             <a className="product__order-button button button--primary button--uppercase" href="##">Оставить заявку</a>
             <a className="product__credit-button button button--uppercase" href="##">В кредит от 11 000 ₽</a>
           </div>
+          <Tabs className="product__details tabs">
+            <TabList className="tabs__list">
+              <Tab className="tabs__button">Характеристики</Tab>
+              <Tab className="tabs__button">Отзывы</Tab>
+              <Tab className="tabs__button">Контакты</Tab>
+            </TabList>
+            <TabPanels className="tabs__panels">
+              <TabPanel className="product__specifications">
+                <table>
+                  <tbody>
+                    { PRODUCT.specifications.map(spec => <tr key={spec.name}>
+                      <td>{ spec.name }</td>
+                      <td>{ spec.value }</td>
+                    </tr>) }
+                  </tbody>
+                </table>
+              </TabPanel>
+              <TabPanel className="tabs__content"></TabPanel>
+              <TabPanel className="product__contacts">
+                <div className="product__contacts-inner">
+                  <p>
+                    <b>Адрес</b>
+                    Санкт-Петербург,<br/>
+                    набережная реки Карповки, дом 5
+                  </p>
+                  <p>
+                    <b>Режим работы</b>
+                    Ежедневно, с 10:00 до 21:00
+                  </p>
+                  <p>
+                    <b>Телефон</b>
+                    <a href="tel:88003335599">8 (800) 333-55-99</a>
+                  </p>
+                  <p>
+                    <b>E-mail</b>
+                    <a href="mailto:info@avto-moto.ru">info@avto-moto.ru</a>
+                  </p>
+                </div>
+                <div className="product__contacts-map"></div>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </section>
       </main>
       <Footer/>
