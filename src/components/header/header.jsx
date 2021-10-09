ab22ec7383f07ebf5293bd3b7b3d4fbc8bbcb710
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-10-10 01:14:45 +0300

    Добавляет название класса компонента в пропсы

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
 import SiteNav from '../site-nav/site-nav';
 import './header.scss';
 
-function Header() {
-  return <header className="header">
+function Header(props) {
+  const { className: headerClassName } = props;
+
+  return <header className={cn('header', headerClassName)}>
     <SiteNav className="header__nav" />
   </header>;
 }
+
+Header.propTypes = {
+  className: PropTypes.string
+};
 
 export default Header;
