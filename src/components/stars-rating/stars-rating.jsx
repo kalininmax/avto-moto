07ef673b163f7fd17a2171a295adb632880d7098
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-10-12 00:58:45 +0300

    Рефакторинг с использованием map-метода

--- src/components/stars-rating/stars-rating.jsx
+++ src/components/stars-rating/stars-rating.jsx
@@ -4,34 +4,36 @@ import cn from 'classnames';
 import './stars-rating.scss';
 import { ReactComponent as StarIcon } from './star-icon.svg';
 
+const RATING = [5, 4, 3, 2, 1]
+
 function StarsRating(props) {
-  const { className: starsRatingClassName } = props;
+  const { value: ratingValue, onChange: onInputChange, className: starsRatingClassName } = props;
 
   return <div className={cn('stars-rating', starsRatingClassName)}>
-      <input className="stars-rating__input visually-hidden" name="rating" id="5-stars" type="radio" required value="5"/>
-      <label htmlFor="5-stars">
-        <StarIcon className="stars-rating__icon" width="27" height="27"/>
-      </label>
-      <input className="stars-rating__input visually-hidden" name="rating" id="4-stars" type="radio" required value="4"/>
-      <label htmlFor="4-stars">
-        <StarIcon className="stars-rating__icon" width="27" height="27"/>
-      </label>
-      <input className="stars-rating__input visually-hidden" name="rating" id="3-stars" type="radio" required value="3"/>
-      <label htmlFor="3-stars">
-        <StarIcon className="stars-rating__icon" width="27" height="27"/>
-      </label>
-      <input className="stars-rating__input visually-hidden" name="rating" id="2-stars" type="radio" required value="2"/>
-      <label htmlFor="2-stars">
-        <StarIcon className="stars-rating__icon" width="27" height="27"/>
-      </label>
-      <input className="stars-rating__input visually-hidden" name="rating" id="1-stars" type="radio" required value="1"/>
-      <label htmlFor="1-stars">
-        <StarIcon className="stars-rating__icon" width="27" height="27"/>
-      </label>
+      { RATING.map(number => {
+        return (<React.Fragment key={number}>
+            <input
+              onChange={onInputChange}
+              className="stars-rating__input visually-hidden"
+              name="rating"
+              id={`${number}-stars`}
+              type="radio"
+              required
+              checked={ratingValue === number}
+              value={`${number}`}
+            />
+            <label htmlFor={`${number}-stars`}>
+              <StarIcon className="stars-rating__icon" width="27" height="27"/>
+            </label>
+          </React.Fragment>
+        )
+      }) }
     </div>
 }
 
 StarsRating.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func,
   className: PropTypes.string
 };
 
