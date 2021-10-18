2287186c948b0b819c5824e7ddce39456369f658
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-10-18 22:39:19 +0300

    Зацикливает фокус внутри окна

--- src/components/modal-window/modal-window.jsx
+++ src/components/modal-window/modal-window.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import './modal-window.scss';
 
 function ModalWindow(props) {
   const { active = false, setActive, className: modalWindowClassName } = props;
+  const modalContentRef = useRef(null);
 
   const onCloseButtonClick = evt => {
     evt.preventDefault();
@@ -12,6 +13,29 @@ function ModalWindow(props) {
   }
 
   useEffect(() => {
+    const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+    const modalContent = modalContentRef.current;
+    let focusableElements = modalContent.querySelectorAll(focusableElementsString);
+    focusableElements = Array.prototype.slice.call(focusableElements);
+    const firstTabStop = focusableElements[0];
+    const lastTabStop = focusableElements[focusableElements.length - 1];    
+    
+    const onModalKeydown = evt => {
+      if (evt.key === 'Tab') {
+        if (evt.shiftKey && document.activeElement === firstTabStop) {
+          evt.preventDefault();
+          lastTabStop.focus();
+        }
+
+        if (document.activeElement === lastTabStop && !evt.shiftKey) {
+          evt.preventDefault();
+          firstTabStop.focus();
+        }
+      }
+    }
+
+    modalContent.addEventListener('keydown', onModalKeydown);
+
     const onEscKeydown = evt => {
       
       if (evt.key === 'Escape') {
@@ -22,12 +46,15 @@ function ModalWindow(props) {
 
     window.addEventListener('keydown', onEscKeydown);
 
-    return () => window.removeEventListener('keydown', onEscKeydown)
+    return () => {
+      window.removeEventListener('keydown', onEscKeydown);
+      modalContent ?? modalContent.current.removeEventListener('keydown', onModalKeydown);
+    }
   }, [active, setActive]);
 
   return <div className={cn(`modal-window ${active ? '' : 'modal-window--hidden'}`, modalWindowClassName)}>
     <div className="modal-window__overlay" onClick={onCloseButtonClick}></div>
-    <div className="modal-window__content">
+    <div ref={modalContentRef} className="modal-window__content">
       { props.children }
       <button onClick={onCloseButtonClick} className="modal-window__button-close" aria-label="Закрыть"></button>
     </div>
